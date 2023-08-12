4f1f8816c36fbf9bb8b0d16f31dc08369bab1803
fbrcode <fabio.bressler@gmail.com>
2023-08-12 15:20:48 -0300

    fix: import reference adjustment

--- infra/src/main.ts
+++ infra/src/main.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { InfraStack } from "./main/app/cdk-stack";
+import { InfraStack } from "./app/cdk-stack";
 
 const app = new cdk.App();
 new InfraStack(app, "InfraStack", {
