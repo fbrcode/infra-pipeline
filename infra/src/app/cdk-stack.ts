b1274e813dff9e1a7207fd12adf67e3a91978310
fbrcode <fabio.bressler@gmail.com>
2023-08-12 14:26:25 -0300

    chore: adjust folder structure

--- infra/src/app/cdk-stack.ts
+++ infra/src/app/cdk-stack.ts
@@ -6,9 +6,9 @@ export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    // example resource
-    const queue = new sqs.Queue(this, "InfraQueue", {
+    // The code that defines your stack
+    // const queue = new sqs.Queue(this, "InfraQueue", { visibilityTimeout: cdk.Duration.seconds(300),});
+    new sqs.Queue(this, "InfraQueue", {
       visibilityTimeout: cdk.Duration.seconds(300),
     });
   }
